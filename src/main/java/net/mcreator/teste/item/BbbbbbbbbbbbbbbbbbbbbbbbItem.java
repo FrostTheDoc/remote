b2f0b41dbe389f4d134d11bd9f7496fc93650c7f
FrostTheDoc <Professores - RT@DESKTOP-E7SSCNR>
2024-10-21 14:28:55 -0300

    bbbbbbbbbbbbbbbbb

---
+++ src/main/java/net/mcreator/teste/item/BbbbbbbbbbbbbbbbbbbbbbbbItem.java
@@ -0,0 +1,11 @@
+
+package net.mcreator.teste.item;
+
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+
+public class BbbbbbbbbbbbbbbbbbbbbbbbItem extends Item {
+	public BbbbbbbbbbbbbbbbbbbbbbbbItem() {
+		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
+	}
+}
